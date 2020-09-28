c18036d515f1112c9d46da530d8504c78977ea75
TKJohn <15013144+TKJohn@users.noreply.github.com>
2020-09-29 00:24:35 +0800

    Ch5. Part 1. Stream

---
+++ src/scala/fpinscla/Stream.scala
@@ -0,0 +1,54 @@
+package fpinscla
+
+import fpinscla.Stream.{cons, empty}
+
+import scala.annotation.tailrec
+import scala.{Stream => _}
+
+sealed trait Stream[+A] {
+  // 5.1
+  def toList: List[A] = this match {
+    case Empty => Nil
+    case Cons(h, t) => h() :: t().toList
+  }
+
+  def toList_2: List[A] = {
+    @tailrec
+    def run(as: Stream[A], acc: List[A]): List[A] = as match {
+      case Empty => acc
+      case Cons(h, t) => run(t(), h() :: acc)
+    }
+
+    run(this, List.empty).reverse
+  }
+
+  // 5.2
+  def take(n: Int): Stream[A] = this match {
+    case Cons(h, t) if n > 1 => cons(h(), t().take(n - 1))
+    case Cons(h, _) if n == 1 => cons(h(), empty)
+    case _ => empty
+  }
+
+  @tailrec
+  final def drop(n: Int): Stream[A] = this match {
+    case Cons(_, t) if n > 0 => t().drop(n - 1)
+    case _ => this
+  }
+
+}
+
+case object Empty extends Stream[Nothing]
+
+case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
+
+object Stream {
+  def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
+    lazy val head = hd
+    lazy val tail = tl
+    Cons(() => head, () => tail)
+  }
+
+  def empty[A]: Stream[A] = Empty
+
+  def apply[A](as: A*): Stream[A] = if (as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
+}
