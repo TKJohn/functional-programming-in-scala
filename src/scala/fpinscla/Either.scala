128335b94a9170f42c1619bbde9578760a335c11
TKJohn <15013144+TKJohn@users.noreply.github.com>
2020-09-28 23:22:18 +0800

    Ch4. Part 2. Either

---
+++ src/scala/fpinscla/Either.scala
@@ -0,0 +1,43 @@
+package fpinscla
+
+//hide std library `Option` and `Either`, since we are writing our own in this chapter
+
+import scala.{Either => _, Option => _}
+
+sealed trait Either[+E, +A] {
+  //4.6
+  def map[B](f: A => B): Either[E, B] = this match {
+    case Left(value) => Left(value)
+    case Right(value) => Right(f(value))
+  }
+
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+    case Left(value) => Left(value)
+    case Right(value) => f(value)
+  }
+
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
+    case Left(_) => b
+    case Right(value) => Right(value)
+  }
+
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = for {
+    ra <- this
+    rb <- b
+  } yield f(ra, rb)
+
+}
+
+case class Left[+E](value: E) extends Either[E, Nothing]
+
+case class Right[+A](value: A) extends Either[Nothing, A]
+
+object Either {
+
+  def traverse[E, A, B](as: List[A])(f: A => Either[E, B]): Either[E, List[B]] = as match {
+    case ::(head, next) => f(head).map2(traverse(next)(f))(_ :: _)
+    case Nil => Right(Nil)
+  }
+
+  def sequence[E, A](es: List[Either[E, A]]): Either[E, List[A]] = traverse(es)(x => x)
+}
