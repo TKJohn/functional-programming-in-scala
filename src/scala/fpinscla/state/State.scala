6f4ab637deb6d6a2ae5a8aea0883225aee63fbc8
TKJohn <15013144+TKJohn@users.noreply.github.com>
2020-10-29 00:16:24 +0800

    Ch 6. Part 2.
    Jump into map.

--- src/scala/fpinscla/state/State.scala
+++ src/scala/fpinscla/state/State.scala
@@ -21,6 +21,16 @@ object RNG {
 
   type Rand[+A] = RNG => (A, RNG)
 
+  val int: Rand[Int] = _.nextInt
+
+  def unit[A](a: A): Rand[A] = rng => (a, rng)
+
+  def map[A, B](s: Rand[A])(f: A => B): Rand[B] =
+    rng => {
+      val (a, rng2) = s(rng)
+      (f(a), rng2)
+    }
+
   // 6.1
   def nonNegativeInt(rng: RNG): (Int, RNG) = {
     val (v, r) = rng.nextInt
@@ -70,4 +80,20 @@ object RNG {
     run(count, rng, List.empty)
   }
 
+  // 6.5
+  val doubleViaMap: Rand[Double] = map(nonNegativeInt)(_ / Int.MaxValue)
+
+  // 6.6
+  def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    rng => {
+      val (va, rng2) = ra(rng)
+      val (vb, rng3) = rb(rng2)
+      (f(va, vb), rng3)
+    }
+
+  // 6.7
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] =
+    fs.foldRight(unit(List.empty[A]))((r, acc) => map2(r, acc)(_ :: _))
+
+  def intsViaSequence(count: Int): Rand[List[Int]] = sequence(List.fill(count)(int))
 }
